2bf25d2c760710569d5a6cdf73febca434a73003
Fanny Rostaing <fanny@coffretis.com>
2020-08-18 16:46:08 +0200

    added rating in start format

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,4 +2,5 @@ class Experience < ApplicationRecord
   belongs_to :user
 
   validates :category, presence: true, inclusion: { in: ["sciences fiction", "histoire", "sensations", "insolite"] }
+
 end
