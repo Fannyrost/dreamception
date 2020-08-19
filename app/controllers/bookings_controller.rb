50d8557edad90630e8e0fa8ef084a1061827a38c
Fanny Rostaing <fanny@coffretis.com>
2020-08-19 15:04:53 +0200

    JS on the go

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,23 @@ class BookingsController < ApplicationController
     @experience = Experience.find(params[:experience_id])
     @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @experience = Experience.find(params[:experience_id])
+    @booking.experience_id = @experience.id
+    @booking.user = current_user
+      if @booking.save
+        redirect_to user_path(@user)
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :total_price, :nb_of_persons)
+  end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :user
+  belongs_to :experiences
+
+  validate :number_of_person
+
+  def number_of_person
+    @experience = Experience.find(params[:experience_id])
+    if nb_of_person > @experience.nb_max_of_persons
+      errors.add(:nb_of_persons, "trop de monde ")
+    end
+  end
 end
