66e9044bc21fe7cad6aa8d9a63d635b71601889f
Noor Makria noor.makria@gmail.com <noor.makria@gmail.com>
2020-08-19 14:32:14 +0200

    To do: one same page show for user and seller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
- 
   def show_user
     @user = User.find(params[:id])
   end
 
+  def show_seller?
+    @user.is_a_seller
+    @user_seller = User.find(params[:id])
+  end
 end
