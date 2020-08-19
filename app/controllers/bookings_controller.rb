295b2662cf89511f6481838cd893f2b3b78a94ec
Fanny Rostaing <fanny@coffretis.com>
2020-08-19 15:10:22 +0200

    checkpoint

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
-<<<<<<< HEAD
+  before_action :authenticate_user!, except: [:show, :index]
+
   def new
     @experience = Experience.find(params[:experience_id])
     @booking = Booking.new
@@ -10,11 +11,12 @@ class BookingsController < ApplicationController
     @experience = Experience.find(params[:experience_id])
     @booking.experience_id = @experience.id
     @booking.user = current_user
-      if @booking.save
-        redirect_to user_path(@user)
-      else
-        render :new
-      end
+
+    if @booking.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   private
@@ -22,11 +24,4 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:date, :total_price, :nb_of_persons)
   end
-
-=======
-  before_action :authenticate_user!, except: [:show, :index]
-  def new
-    @booking = Booking.new
-  end
->>>>>>> d124a5218fd9c099f72ef415c1ef688cf465c929
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,8 +5,7 @@ class Booking < ApplicationRecord
   validate :number_of_person
 
   def number_of_person
-    @experience = Experience.find(params[:experience_id])
-    if nb_of_person > @experience.nb_max_of_persons
+    if nb_of_person > experience.nb_max_of_persons
       errors.add(:nb_of_persons, "trop de monde ")
     end
   end
