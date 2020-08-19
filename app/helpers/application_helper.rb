3387c4b0232da6587c467ccb5667425c3c1749e7
Hugo Fortunier <hfortunier@gmail.com>
2020-08-19 18:45:25 +0200

    affichage des photos à jour, mais necessite encore du traitement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def photo_for(experience)
+    if experience.photo.attached?
+      cl_image_path experience.photo.key, height: 200, width: 160, crop: :fill
+    else
+      image_path 'http://unsplash.it/400/400'
+    end
+  end
 end
