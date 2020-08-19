2f79357f5196cc218b188d3197154227fdac5266
Emma Colombat <emma.colombat69@gmail.com>
2020-08-19 19:15:20 +0200

    finished devise

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'navy-card': '#023148',
-        'creme-card': '#D9D9D9'
+        'navy-card': '#304261',
+        'creme-card': '#D9D9D9',
       }
     }
   },
