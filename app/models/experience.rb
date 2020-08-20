c8081225de53ab2656017f3a4d556f167a3b9f93
Emma Colombat <emma.colombat69@gmail.com>
2020-08-20 14:57:28 +0200

    updated models review, user, experience

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,6 +2,7 @@ class Experience < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews
+  has_many :users
   has_one_attached :photo
 
   validates :category, presence: true, inclusion: { in: ["sciences fiction", "histoire", "sensations", "insolite"] }
