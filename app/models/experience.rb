df58d34f77fdc5075cd233de77556aee16b17d09
Emma Colombat <emma.colombat69@gmail.com>
2020-08-20 17:25:34 +0200

    has_manu user no more

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,7 +2,6 @@ class Experience < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews
-  has_many :users
   has_one_attached :photo
 
   validates :category, presence: true, inclusion: { in: ["sciences fiction", "histoire", "sensations", "insolite"] }
