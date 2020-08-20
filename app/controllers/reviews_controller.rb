4da28f3c45f5860e2b66215954deba34dacff6c1
Emma Colombat <emma.colombat69@gmail.com>
2020-08-20 17:23:04 +0200

    created review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,28 @@
 class ReviewsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def new
+    @experience = Experience.find(params[:experience_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @experience = Experience.find(params[:experience_id])
+    @review.experience = @experience
+    @review.user = current_user
+      if @review.save
+        redirect_to users_profile_path
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+
+  end
+
 end
